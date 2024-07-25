24f31d9096bbe19b535656aff6240292e0c9e658
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-07-26 06:32:09 +0900

    $manifestFile

--- .github/workflows/build-manifest-json.php
+++ .github/workflows/build-manifest-json.php
@@ -9,6 +9,7 @@ $options = [
 $context = stream_context_create($options);
 $html = file_get_contents($url, false, $context);
 $github = json_decode($html, TRUE);
-$manifest = __DIR__ . '/../../lib/manifest.json';
-$manifest = json_decode(file_get_contents( realpath($manifest) ), TRUE);
-var_dump(json_encode(array_merge($manifest, ['updated_at'=>time()])));
+$manifestFile = __DIR__ . '/../../lib/manifest.json';
+$manifest = json_decode(file_get_contents( realpath($manifestFile) ), TRUE);
+
+file_put_contents($manifestFile, json_encode(array_merge($manifest, ['updated_at'=>time()])));
