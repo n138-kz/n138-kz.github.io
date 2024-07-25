4dafb1c5f25b0ef06bcff3b88b11747f4d014962
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-07-25 19:32:21 +0900

    Update build-manifest-json.php

--- .github/workflows/build-manifest-json.php
+++ .github/workflows/build-manifest-json.php
@@ -1,11 +1,6 @@
 <?php
-$options = [
-    'http' => [
-        'method' => 'GET',
-        'header' => 'curl',
-    ],
-];
-$context = stream_context_create($options);
+$curl = curl_init('https://api.github.com/repos/n138-kz/n138-kz.github.io');
+echo $curl->curl_exec() . PHP_EOL . PHP_EOL . PHP_EOL;
 
 $github = json_decode(file_get_contents('https://api.github.com/repos/n138-kz/n138-kz.github.io', false, $context), TRUE);
 $manifest = json_decode(file_get_contents('../.././lib/manifest.json'), TRUE);
