4c4fc5110eb684237812bf8bf0a4eba44c3fa270
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-07-26 06:17:09 +0900

    Fix PHP Parse error: syntax error

--- .github/workflows/build-manifest-json.php
+++ .github/workflows/build-manifest-json.php
@@ -9,6 +9,6 @@ $options = [
 $context = stream_context_create($options);
 $html = file_get_contents($url, false, $context);
 
-$github = json_decode($html), TRUE);
+$github = json_decode($html, TRUE);
 $manifest = json_decode(file_get_contents('../.././lib/manifest.json'), TRUE);
 var_dump(json_encode(array_merge($manifest, ['updated_at'=>time()])));
