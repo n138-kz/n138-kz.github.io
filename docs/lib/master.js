e570acf132f24cfe2fad9ca0195a88977fc719b8
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-03-18 21:31:40 +0900

    無効表示のAD無効化 (#39)
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Create ad.css
    
    * Create ad.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js
    
    * Update master.js

--- docs/lib/master.js
+++ docs/lib/master.js
@@ -156,33 +156,37 @@ window.addEventListener('DOMContentLoaded', (e)=>{
 	});
 });
 window.addEventListener('load', ()=>{
-	let list=[];
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892200357'); /* 251023_Amazon.co.jp通販サイト(アマゾン)_728x90 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562209'); /* 260308_ソフトバンクオンラインショップ_728x90 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562211'); /* 260308_総合ネット通販 タンタンショップ_300x250 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562212'); /* 260308_障がい者就職支援受付窓口_300x250 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562213'); /* 260308_トリファ_300x250 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562214'); /* 260308_トリファ_300x250_1 */
-	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892562215'); /* 260308_トリファ_300x250_2 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566288'); /* 260316_FRONTIERダイレクト_468x60 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566289'); /* 260316_NEC Direct(NECダイレクト)_728x90 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566290'); /* 260316_ソースネクスト_728x90 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566291'); /* 260316_Renta！_728x90 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566292'); /* 260316_Renta！_728x90_1 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566294'); /* 260316_オーディオブック配信 audiobook.jp_728x90 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566295'); /* 260316_オーディオブック配信 audiobook.jp_728x90_1 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566296'); /* 260316_オーディオブック配信 audiobook.jp_728x90_2 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/smartphonebanner?sid=3755829&pid=892566293&position=overlay'); /* 260316_オーディオブック配信 audiobook.jp_320x50 */
+	{
+		let item = null;
+		item = document.createElement('script');
+		item.src = 'https://n138-kz.github.io/lib/ad.js';
+		document.head.appendChild(item);
+		item = document.createElement('link');
+		item.rel = 'stylesheet';
+		item.href = 'https://n138-kz.github.io/lib/ad.css';
+		document.head.appendChild(item);
 
-	let url=list[getRandomIntInclusive(0,list.length-1)];
-	const container = document.createElement('div');
-	document.body.prepend(container);
-
-	/* postscribeを使って、そのdivの中にスクリプトを書き込む */
-	/* 第一引数：書き込み先の要素 */
-	/* 第二引数：実行したいHTML（scriptタグ） */
-	postscribe(container, `<script src="${url}"></script>`);
-	console.debug(`Ad link has set: ${url}`);
+		setTimeout(() => {
+			document.querySelectorAll('script[src^="//ad.jp.ap.valuecommerce.com"]+a>img').forEach((e)=>{
+				e.remove();
+			});
+			try {
+				initAd();
+			} catch (error) {
+				console.error(error);
+			}
+		}, 1*1000);
+		setInterval(() => {
+			document.querySelectorAll('script[src^="//ad.jp.ap.valuecommerce.com"]+a>img').forEach((e)=>{
+				e.remove();
+			});
+			try {
+				initAd();
+			} catch (error) {
+				console.error(error);
+			}
+		}, 60*1000);
+	}
 });
 window.addEventListener('load', ()=>{
 	const social_icon = document.createElement('div');
