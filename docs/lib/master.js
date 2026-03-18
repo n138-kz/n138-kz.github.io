a4648580f93d5f573ce985699835450486afe71d
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-03-18 18:30:21 +0900

    Update master.js

--- docs/lib/master.js
+++ docs/lib/master.js
@@ -170,7 +170,7 @@ window.addEventListener('load', ()=>{
 	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566291'); /* 260316_Renta！_728x90 */
 	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566292'); /* 260316_Renta！_728x90_1 */
 	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566294'); /* 260316_オーディオブック配信 audiobook.jp_728x90 */
-	list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566295'); /* 260316_オーディオブック配信 audiobook.jp_728x90_1 */
+	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566295'); /* 260316_オーディオブック配信 audiobook.jp_728x90_1 */
 	/* list.push('//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=3755829&pid=892566296'); /* 260316_オーディオブック配信 audiobook.jp_728x90_2 */
 	list.push('//ad.jp.ap.valuecommerce.com/servlet/smartphonebanner?sid=3755829&pid=892566293&position=overlay'); /* 260316_オーディオブック配信 audiobook.jp_320x50 */
 
