c48c50b7b5b9195e86f7792f7aef6941f8b6edc6
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-03-18 18:53:48 +0900

    Update master.js

--- docs/lib/master.js
+++ docs/lib/master.js
@@ -166,13 +166,16 @@ window.addEventListener('load', ()=>{
 		item.href = 'https://n138-kz.github.io/lib/ad.css';
 		document.head.appendChild(item);
 
-		while (true) {
-			/* 例外(ReferenceError: initAd is not defined)が発生したら、もう一度やり直す。 */
-			try {
-				initAd();
-				break;
-			} catch (error) {
-				console.error(error);
+		{
+			while (typeof loop_breaker === 'undefined') {
+				/* 例外(ReferenceError: initAd is not defined)が発生したら、もう一度やり直す。 */
+				try {
+					initAd();
+					loop_breaker=true;
+					break;
+				} catch (error) {
+					console.error(error);
+				}
 			}
 		}
 
@@ -180,10 +183,11 @@ window.addEventListener('load', ()=>{
 			document.querySelectorAll('script[src^="//ad.jp.ap.valuecommerce.com"]+a>img').forEach((e)=>{
 				e.remove();
 			});
-			while (true) {
+			while (typeof loop_breaker === 'undefined') {
 				/* 例外(ReferenceError: initAd is not defined)が発生したら、もう一度やり直す。 */
 				try {
 					initAd();
+					loop_breaker=true;
 					break;
 				} catch (error) {
 					console.error(error);
