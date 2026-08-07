5466cbc7dcb4f198ade875032be8af88e9a94442
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-07 12:52:27 +0900

    Add debug logs for token in grecaptcha_pickup (#44)
    * Add debug logs for token in grecaptcha_pickup
    
    Add console logs for debugging token retrieval
    
    * Enhance token validation checks
    
    * Improve token validation checks in grecaptcha.js
    
    Refactor token validation logic to check for undefined and null separately.
    
    * Add googleRecaptcha class for token management
    
    Implemented googleRecaptcha class to handle token generation and storage.
    
    * Enhance comments in googleRecaptcha class
    
    Added comments for clarity in googleRecaptcha class.

--- docs/lib/grecaptcha.js
+++ docs/lib/grecaptcha.js
@@ -38,8 +38,16 @@ function grecaptcha_init(act = 'homepage'){
 function grecaptcha_pickup(){
 	try {
 		token = localStorage.getItem( (btoa(location.href)).slice(0, 16) + '.reCAPTCHA' );
+		console.log(token);
+		console.log(typeof token);
 		
-		if( typeof token === 'undefined' || token.length == 0 ){
+		if( typeof token === 'undefined' ){
+			throw 'No token';
+		}
+		if( typeof token === 'object' && token === null ){
+			throw 'No token';
+		}
+		if( token.length == 0 ){
 			throw 'No token';
 		}
 		return token;
@@ -49,3 +57,41 @@ function grecaptcha_pickup(){
 		return null;
 	}
 }
+/* **********+**********+********** */
+/**
+class googleRecaptcha {
+	constructor(sitekey) {
+		this.client_id = sitekey;
+	}
+	init() {
+		grecaptcha.execute(this.client_id, {action: 'user/register'}).then((token)=>{
+			expire_at = {
+				issued_at: new Date().getTime(),
+				expire_at: new Date(new Date().getTime()+(5*60*1000)).getTime(),
+				/* expire_at: 5min */ /**
+			}
+			console.debug(expire_at);
+			localStorage.setItem( (btoa(location.href)).slice(0, 16) + '.reCAPTCHA', {
+				issued_at: expire_at.issued_at,
+				expire_at: expire_at.expire_at,
+				token: token,
+			} );
+		});
+	}
+	getToken() {
+		const token = localStorage.getItem( (btoa(location.href)).slice(0, 16) + '.reCAPTCHA' );
+		if( typeof token === 'undefined' ){
+			return null;
+		}
+		if( typeof token === 'object' && token === null ){
+			return null;
+		}
+		if( token.length == 0 ){
+			return null;
+		}
+		return token;
+	}
+}
+googleRecaptcha = new googleRecaptcha('6LfCHdcUAAAAAOwkHsW_7W7MfoOrvoIw9CXdLRBA');
+googleRecaptcha.init();
+/* // **/
