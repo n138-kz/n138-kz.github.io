db2e29ab4ceb5bd25d5805edb45ea310726157fa
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-07 12:27:13 +0900

    Improve token validation checks in grecaptcha.js
    Refactor token validation logic to check for undefined and null separately.

--- docs/lib/grecaptcha.js
+++ docs/lib/grecaptcha.js
@@ -41,7 +41,13 @@ function grecaptcha_pickup(){
 		console.log(token);
 		console.log(typeof token);
 		
-		if( typeof token === 'undefined' || token === null || token.length == 0 ){
+		if( typeof token === 'undefined' ){
+			throw 'No token';
+		}
+		if( typeof token === 'object' && token === null ){
+			throw 'No token';
+		}
+		if( token.length == 0 ){
 			throw 'No token';
 		}
 		return token;
